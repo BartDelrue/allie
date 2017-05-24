c704889d720c65d9573884aed47b050e8ce1ab8f
Bart <delrue.bart@gmail.com>
2017-05-24 14:44:12 +0200

    navi-drawer setup + webpack config

--- src/components/_navigation-drawer.js
+++ src/components/_navigation-drawer.js
@@ -1,15 +1,16 @@
 let naviDrawer = (() => {
   'use strict';
 
-  let overlay = document.querySelector('[data-modal-overlay');
-  let triggers = document.querySelectorAll('input[type=button][data-modal-trigger], button[data-modal-trigger');
+  let overlay = document.querySelector('[data-navi-drawer-overlay]');
+  let triggers = document.querySelectorAll('input[type=button][data-navi-drawer-trigger], button[data-navi-drawer-trigger]');
   let closeBtns = document.querySelectorAll('[data-modal-close');
   let trigger, target, focusableElems;
   let focusPosition = -1;
-  let bodyChildren = document.querySelectorAll('body > *:not([data-modal])');
+  let bodyChildren = document.querySelectorAll('body > *:not([data-navi-drawer])');
 
   let smooth = true;
   let duration = 0.75;
+  let width, closeNavi, openNavi;
 
   let setDuration = function (d) {
     duration = d;
@@ -18,12 +19,18 @@ let naviDrawer = (() => {
     smooth = (bool === true);
   };
 
+  //http://gizma.com/easing/
   Math.easeInQuad = function (t, b, c, d) {
     t /= d;
     return c * t * t * t + b;
   };
 
-  let naviTriggers = document.querySelectorAll('input[type=button][data-navi-drawer-trigger], button[data-navi-drawer-trigger');
+  Math.easeOutQuad = function (t, b, c, d) {
+    t /= d;
+    return -c * t * (t - 2) + b;
+  };
+  //
+
 
   let handleKeyboardInput = (e) => {
 
@@ -53,6 +60,60 @@ let naviDrawer = (() => {
 
   };
 
+  let open = function (target, t, isIn, next) {
+    let left = Math.easeOutQuad(t, 0, width, duration);
+
+    console.log(left);
+    if (left >= width && next)
+      next(isIn);
+    else target.style.left = isIn ? -(width - left) + 'px' : -left + 'px';
+  };
+
+  // let open = function (target, t) {
+  //   let left = -(width - Math.easeOutQuad(t, 0, width, duration));
+  //
+  //   if (left >= 0) {
+  //     target.removeAttribute('style');
+  //     if (openNavi)
+  //       clearInterval(openNavi);
+  //   }
+  //   else
+  //     target.style.left = left + 'px';
+  // };
+  //
+  // let close = function (target, t) {
+  //   let left = -Math.easeOutQuad(t, 0, width, duration);
+  //
+  //   if (left <= -width) {
+  //     target.removeAttribute('style');
+  //     target.hidden = true;
+  //     if (closeNavi)
+  //       clearInterval(closeNavi);
+  //   }
+  //   else
+  //     target.style.left = left + 'px';
+  // };
+
+  let fade = function (elem, t, isIn, next) {
+
+    let opacity = Math.easeOutQuad(t, 0, 1, duration);
+
+    if (opacity >= 1 && next)
+      next(isIn);
+    else elem.style.opacity = isIn ? opacity : 1 - opacity;
+
+  };
+
+  let animationFinished = function (isOpen) {
+    target.removeAttribute('style');
+    overlay.removeAttribute('style');
+    if (closeNavi) clearInterval(closeNavi);
+    if (openNavi) clearInterval(openNavi);
+
+    overlay.hidden = !isOpen;
+    target.hidden = !isOpen;
+  };
+
   let openDrawer = (e) => {
 
     if (e) e.preventDefault();
@@ -60,40 +121,30 @@ let naviDrawer = (() => {
     trigger = e.target;
     target = document.querySelector('nav[data-navi-drawer]');
 
-    if (target) {
+    if (target && overlay) {
+
+      target.hidden = false;
+      overlay.hidden = false;
 
       if (smooth) {
 
-        target.style.display = 'block';
-        let width = target.getBoundingClientRect().width;
-
+        width = target.getBoundingClientRect().width;
         target.style.left = -width + 'px';
-
-        let open = function (target, t) {
-          let left = -(width - Math.easeInQuad(t, 0, width, duration));
-
-          if (left >= 0) {
-            target.removeAttribute('style');
-            clearInterval(openNavi);
-          }
-          else
-            target.style.left = left + 'px';
-        };
+        overlay.style.opacity = 0;
 
         let t = 0;
-        let openNavi = setInterval(function () {
-            open(target, (++t * 100) / 1000);
+        openNavi = setInterval(function () {
+            open(target, (++t * 50) / 1000, true, animationFinished);
+            fade(overlay, (++t * 50) / 1000, true, animationFinished);
           }, 50
         );
       }
+
       target.className += ' navi-drawer--visible';
+      target.tabIndex = -1;
+      target.focus();
+      overlay.addEventListener('click', closeDrawer);
 
-
-      // target.hidden = !modalTarget.hidden;
-      // target.hidden = !modalOverlay.hidden;
-      // target.tabIndex = -1;
-      // target.focus();
-      //
       // focusableElems = modalTarget.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), [tabindex="0"]');
       // focusableElems = Array.prototype.slice.call(focusableElems);
       //
@@ -105,32 +156,40 @@ let naviDrawer = (() => {
     }
   };
 
-  let closeModal = (e) => {
+  let closeDrawer = (e) => {
 
     if (e) e.preventDefault();
 
-    modalTarget.hidden = true;
-    modalOverlay.hidden = true;
+    if(target && overlay){
+      overlay.removeEventListener('click', closeDrawer);
 
-    document.removeEventListener('keydown', handleKeyboardInput);
+      if (smooth) {
 
-    bodyChildren.forEach(child => {
-      child.setAttribute('aria-hidden', false);
-    });
+        let t = 0;
+        closeNavi = setInterval(function () {
+            open(target, (++t * 50) / 1000, false, animationFinished);
+            fade(overlay, (++t * 50) / 1000, false, animationFinished);
+          }, 50
+        );
+      }
 
-    modalTrigger.focus();
+      document.removeEventListener('keydown', handleKeyboardInput);
+
+      bodyChildren.forEach(child => {
+        child.setAttribute('aria-hidden', false);
+      });
+
+      trigger.focus();
+    }
   };
 
-  naviTriggers.forEach(trigger => {
+  triggers.forEach(trigger => {
     trigger.addEventListener('click', openDrawer);
   });
 
   closeBtns.forEach(closeBtn => {
-    closeBtn.addEventListener('click', closeModal);
+    closeBtn.addEventListener('click', closeDrawer);
   });
-
-  if (overlay)
-    overlay.addEventListener('click', closeModal);
 
 
   return {
