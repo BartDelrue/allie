a955fde22614336b6378b60017a8c2522bf67770
Bart <delrue.bart@gmail.com>
2017-05-26 14:40:43 +0200

    typo

--- src/allie.js
+++ src/allie.js
@@ -1,9 +1,4 @@
-/**
- * Created by Bart on 20/05/2017.
- */
-
 module.exports = {
-  accordion: require('./components/_accordion'),
   modal: require('./components/_modal'),
   naviDrawer: require('./components/_navigation-drawer'),
   tabs: require('./components/_tabs')
